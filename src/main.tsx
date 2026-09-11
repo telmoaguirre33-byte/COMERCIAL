09475b434f2140e4d7a58f2e21662fe4d664a508
telmoaguirre33-byte <telmoaguirre33@gmail.com>
2026-09-10 21:22:29 -0300

    feat: require authenticated SIGO session

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import SigoAuthGate from "./SigoAuthGate";
 import SigoRoot from "./SigoRoot";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SigoRoot />
+    <SigoAuthGate>
+      <SigoRoot />
+    </SigoAuthGate>
   </React.StrictMode>
 );
