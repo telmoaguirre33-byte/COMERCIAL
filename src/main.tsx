9860a57b3301726be3909f081db1fbfca7282a0d
telmoaguirre33-byte <telmoaguirre33@gmail.com>
2026-09-12 18:23:47 -0300

    feat: load mobile scanner presentation

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import SigoAuthGate from "./SigoAuthGate";
 import SigoRoot from "./SigoRoot";
 import "./index.css";
+import "./barcode-scanner.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
