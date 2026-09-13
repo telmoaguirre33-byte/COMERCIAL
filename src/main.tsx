79642089eafe258b41244db154d1d61bdb5cbf61
telmoaguirre33-byte <telmoaguirre33@gmail.com>
2026-09-12 21:43:39 -0300

    ui: load native app presentation layer

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import SigoRoot from "./SigoRoot";
 import "./index.css";
 import "./barcode-scanner.css";
 import "./premium-mobile.css";
+import "./native-app.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
